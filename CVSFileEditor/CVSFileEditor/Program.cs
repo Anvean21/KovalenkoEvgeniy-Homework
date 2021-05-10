95dabc3641bd5d5d8d37b1fd9d3a9ccf5b72a9e5
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-05-10 15:59:57 +0300

    CSV file editor - finished

--- CVSFileEditor/CVSFileEditor/Program.cs
+++ CVSFileEditor/CVSFileEditor/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using CsvHelper;
+using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Linq;
 
 namespace CVSFileEditor
 {
@@ -6,7 +15,38 @@ namespace CVSFileEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var persons = PersonList.GetListPerson();
+
+            var csv = new StringBuilder();
+
+            Console.WriteLine("Введите поля");
+            var str = Console.ReadLine().Split(",");
+            foreach (var item in persons)
+            {
+                Print(() => item.Name, str, csv);
+                Print(() => item.Address, str, csv);
+                Print(() => item.Age, str, csv);
+                Print(() => item.EyeColor, str, csv);
+                Print(() => item.Gender, str, csv);
+                Print(() => item.Salary, str, csv);
+                Print(() => item.Company, str, csv);
+                csv.Append("\n");
+            }
+            File.AppendAllText("D:\\file.csv", csv.ToString());
+
+        }
+        static void Print<T>(Expression<Func<T>> expression, string[] str, StringBuilder csv)
+        {
+
+            foreach (var item in str)
+            {
+                if (item == ((MemberExpression)expression.Body).Member.Name)
+                {
+                    Console.WriteLine("Цикл отработал - " + item);
+                    csv.Append(((MemberExpression)expression.Body).Member.Name + " - " + expression.Compile()());
+                    csv.Append(",");
+                }
+            }
         }
     }
 }
