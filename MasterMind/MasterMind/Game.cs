9fd9649cd23b2169b1f4961038772aa9b88ccd1a
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-05-16 20:59:12 +0300

    CowAndBulls

---
+++ MasterMind/MasterMind/Game.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BullsAndCows
+{
+    public static class Game
+    {
+        static List<string> generalAnswers = GetAllAnswers();
+
+        public static void ComputerVersusPlayer()
+        {
+            string currentAnswer = GetOneAnswer(generalAnswers);
+            List<string> currentPossibleAnswers = generalAnswers;
+
+            Console.WriteLine($"Может ваше число {currentAnswer} ?");
+
+            Console.Write("Cколько быков? ");
+            int bulls = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько коров? ");
+            int cows = Convert.ToInt32(Console.ReadLine());
+
+            if (bulls < 0 || bulls > 4 || cows < 0 || cows > 4)
+            {
+                Console.WriteLine("Число быков или коров должно быть не меньше 0 и не больше 4х ");
+                return;
+            }
+            else if (bulls == 4)
+            {
+                Console.WriteLine($"Ваше число - {currentAnswer}!");
+                return;
+            }
+
+            generalAnswers = AddAnswer(currentAnswer, bulls, cows, currentPossibleAnswers);
+
+            if (generalAnswers.Count == 1)
+            {
+                Console.WriteLine($"Ваше число - {generalAnswers[0]}!");
+            }
+            else
+            {
+                ComputerVersusPlayer();
+            }
+        }
+        private static List<string> AddAnswer(string currentAnswer, int bulls, int cows, List<string> currentPossibleAnswers)
+        {
+            List<string> newAnswers = new List<string>();
+            for (int i = 0; i < currentPossibleAnswers.Count; i++)
+            {
+                var bullAndCows = CheckBullsAndCows(currentAnswer, currentPossibleAnswers[i]);
+
+                if (bulls == bullAndCows[0] && cows == bullAndCows[1])
+                {
+                    newAnswers.Add(currentPossibleAnswers[i]);
+                }
+            }
+            return newAnswers;
+        }
+        private static int[] CheckBullsAndCows(string currentAnswer, string answerFromPossibleMassive)
+        {
+            char[] current = currentAnswer.ToCharArray();
+            char[] possible = answerFromPossibleMassive.ToCharArray();
+            int bulls = 0;
+            int cows = 0;
+
+            for (int i = 0; i < current.Length; i++)
+            {
+                if (current[i] == possible[i])
+                {
+                    bulls++;
+                }
+                else
+                {
+                    for (int j = 0; j < possible.Length; j++)
+                    {
+                        if (current[i] == possible[j] && current[i] != possible[i])
+                        {
+                            cows++;
+                        }
+                    }
+                }
+            }
+            return new[] { bulls, cows };
+        }
+        static List<string> GetAllAnswers()
+        {
+            var answers = new List<string>();
+            for (int i = 0; i < 10000; i++)
+            {
+                StringBuilder sb = new StringBuilder();
+                if (i.ToString().Length == 1)
+                {
+                    answers.Add(sb.Append("000" + i).ToString());
+                }
+                else if (i.ToString().Length == 2)
+                {
+                    answers.Add(sb.Append("00" + i).ToString());
+                }
+                else if (i.ToString().Length == 3)
+                {
+                    answers.Add(sb.Append("0" + i).ToString());
+                }
+                else if (i.ToString().Length == 4)
+                {
+                    answers.Add(sb.Append(i).ToString());
+                }
+            }
+            return answers;
+        }
+        static string GetOneAnswer(List<string> answers)
+        {
+            var index = new Random().Next(answers.Count);
+            return answers[index];
+        }
+    }
+}
+
---
+++ MasterMind/MasterMind/Program.cs
@@ -0,0 +1,13 @@
+﻿using BullsAndCows;
+using System;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.ComputerVersusPlayer();
+        }
+    }
+}
