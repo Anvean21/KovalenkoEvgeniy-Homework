41aa915f1d3628386034835fd73c7e02e794e0d3
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-05-16 20:48:52 +0300

    Another solution

---
+++ CsvEditor/CsvEditor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CsvEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = PeopleList.GetPeople();
+            var personsProperties = typeof(Person).GetProperties();
+            Console.WriteLine("Введите поля");
+            var userProperties = Console.ReadLine().Split(",");
+            foreach (var property in personsProperties)
+            {
+                foreach (var usersPropertie in userProperties)
+                {
+                    using (StreamWriter sw = new StreamWriter("file.csv", true))
+                    {
+                        if (property.Name == usersPropertie)
+                        {
+                            var fileprop = string.Join("\n", people.Select(x => typeof(Person).GetProperty(usersPropertie).GetValue(x) + " - " + property.Name)) + "\n";
+                            sw.WriteLine(fileprop);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
