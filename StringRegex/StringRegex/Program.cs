e1b8bf1c8b08f4df2320ea7efa0d403712fe13dc
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-05-23 13:43:14 +0300

    StringAndRegextTask

---
+++ StringRegex/StringRegex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
