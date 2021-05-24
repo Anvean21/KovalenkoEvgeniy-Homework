0b5ff0cfd08d9e74b112f9dc434666fad7e6c92f
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-05-24 21:45:36 +0300

    Threads

---
+++ Threads/Threads/Helpers.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    public static class Helpers
+    {
+        static readonly Mutex mutex = new Mutex();
+        static decimal rnd = 0;
+        private static readonly Random random = new Random();
+
+        public static void FillArray(Data methData)
+        {
+            for (int i = methData.Start.Value; i < methData.End.Value; i++)
+            {
+                mutex.WaitOne();
+                rnd = random.Next(100);
+                mutex.ReleaseMutex();
+
+                methData.Array[i] = rnd;
+            }
+        }
+
+        public static void CopyArray(Data methData)
+        {
+            for (int i = methData.Start.Value; i < methData.End.Value; i++)
+            {
+                methData.Array[i] = methData.IntermediateArray[i + methData.Indent.Value];
+            }
+        }
+
+        public static void GetMinimum(Data methData)
+        {
+            decimal min = methData.Array.First();
+            for (int i = methData.Start.Value + 1; i < methData.End.Value; i++)
+            {
+                if (methData.Array[i] < min)
+                {
+                    min = methData.Array[i];
+                }
+            }
+            methData.IntermediateArray[methData.Indent.Value] = min;
+        }
+
+        public static void GetAverage(Data methData)
+        {
+            for (int i = methData.Start.Value + 1; i < methData.End.Value; i++)
+            {
+                methData.IntermediateArray[methData.Indent.Value] += methData.Array[i];
+            }
+            methData.IntermediateArray[methData.Indent.Value] /= methData.End.Value - methData.Start.Value + 1;
+        }
+    }
+}
---
+++ Threads/Threads/ThreadingTask.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Threads
+{
+    public static class ThreadingTask
+    {
+        static readonly int proccessors = Environment.ProcessorCount;
+        static readonly Thread[] threads = new Thread[proccessors];
+
+        public static void Execute(ParameterizedThreadStart parameterizedThreadStart, Data data)
+        {
+            for (int threadNumber = 0; threadNumber < proccessors; threadNumber++)
+            {
+                threads[threadNumber] = new Thread(parameterizedThreadStart);
+
+                var tempData = new Data()
+                {
+                    Array = data.Array,
+                    Start = (data.Start == null) ? threadNumber * data.Array.Length / proccessors : data.Start,
+                    End = (data.End == null) ? (threadNumber + 1) * data.Array.Length / proccessors : data.End,
+                    IntermediateArray = data.IntermediateArray,
+                    Indent = (data.Indent == null) ? threadNumber : data.Indent
+                };
+
+                threads[threadNumber].Start(tempData);
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        public static decimal[] CreateAndFillArray(int arrayLenght)
+        {
+            var randomArray = new decimal[arrayLenght];
+            var methData = new Data() { Array = randomArray };
+
+            Execute((tempData) => Helpers.FillArray((Data)tempData), methData);
+
+            return randomArray;
+        }
+
+        public static decimal[] CreateAndCopyArray(decimal[] basicArray, int startIndex, int endIndex)
+        {
+            var temporaryCopiedArray = new decimal[endIndex - startIndex + 1];
+            var methData = new Data() { Array = temporaryCopiedArray, Indent = startIndex, IntermediateArray = basicArray };
+
+            Execute((tempData) => Helpers.CopyArray((Data)tempData), methData);
+
+            return temporaryCopiedArray;
+        }
+
+        public static decimal GetArrayMin(decimal[] array)
+        {
+            var metdData = new Data() { Array = array, IntermediateArray = new decimal[proccessors] };
+
+            Execute((tempData) => Helpers.GetMinimum((Data)tempData), metdData);
+
+            return metdData.IntermediateArray.Min();
+        }
+
+        public static decimal GetArrayAverage(decimal[] array)
+        {
+            var methData = new Data() { Array = array, IntermediateArray = new decimal[proccessors] };
+
+            Execute((tempData) => Helpers.GetAverage((Data)tempData), methData);
+
+            return methData.IntermediateArray.Average();
+        }
+    }
+}
