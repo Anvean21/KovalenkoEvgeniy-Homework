fea524dd649fec882f98d0a070d550f00e2ce022
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-05-26 17:18:54 +0300

    fixed.

---
+++ ConsoleApp8/DAL/Services/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Core.Models;
+using DAL.Abstractions.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace DAL.Services
+{
+    public class UserRepository : IRepository
+    {
+        public ICollection<User> LoadRecords()
+        {
+            List<User> users = new List<User>();
+
+            MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(GetJsonData()));
+
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(users.GetType());
+
+            users = ser.ReadObject(ms) as List<User>;
+
+            ms.Close();
+            return users;
+        }
+
+        private static string GetJsonData()
+        {
+            string json = "[{\"FirstName\":\"Fred\",\"LastName\":\"Smith\",\"Age\":\"40\"}]";
+            return json;
+        }
+    }
+}
---
+++ ConsoleApp8/TryToFixEverything/Program.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using BLL.Abstractions.Interfaces;
+using BLL.Services;
+using DAL.Abstractions.Interfaces;
+using DAL.Services;
+using System;
+
+namespace ConsoleApp8
+{
+    // Task:
+    // Program should return "Matching Record, got name=Fred, lastname=Smith, age=40"
+    // Могут быть ошибки любого вида:
+    // - отсутствие ссылок на проекты, классы, интерфейсы или же ссылки на несуществующие классы, методы, интерфейсы
+    // - ошибки в нейминге
+    // - ошибки в логике работы программы
+    // - Ошибки в версии целевого фреймворка
+    // - очепятки
+    // - ошибки в DI контейнере
+    // - и т.д.
+    // Необходимо исправить все проблемы и ошибки системы и сделать так, чтобы программа заработала
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<UserService>().As<IUserService>();
+            builder.RegisterType<UserRepository>().As<IRepository>();
+
+            using (var scope = builder.Build().BeginLifetimeScope())
+            {
+                var users = scope.Resolve<IUserService>().LoadRecords();
+
+                foreach (var user in users)
+                {
+                    Console.WriteLine($"Matching Record, got name={user.FirstName}, lastname={user.LastName}, age={user.Age}");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
