cf4bc759681e699a69f3128f0868aeacfb7b8a54
Anvean21 <evgeniy.kovalenko21@gmail.com>
2021-06-06 14:01:14 +0300

    ADO task

---
+++ SqlQueries/ADO.Task/ADO.Task/Entities/SalesOrderDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.Task.Entities
+{
+    public class SalesOrderDetail
+    {
+        public int SalesOrderId { get; set; }
+        public int ProductId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal UnitPriceDiscount { get; set; }
+    }
+}
---
+++ SqlQueries/ADO.Task/ADO.Task/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server =DESKTOP-DKVRESV;Database=AdventureWorksLT2019;Trusted_Connection=True;";
+            Queries queries = new Queries(connectionString);
+
+            var date = DateTime.Now.ToString("yyyy.mm.dd");
+            var query1 = queries.GetCategorySoldProducts(date);
+            foreach (var item in query1)
+            {
+                Console.WriteLine($"{item.ProductCategoryId}\t{item.CategoryName}\t{item.SummaryPrice}");
+            }
+
+            int discount = 4;
+            var query2 = queries.CustomersViaDiscount(discount);
+            foreach (var item in query2)
+            {
+                Console.WriteLine($"{item.CustomerId}\t{item.Title}\t{item.FirstName}\t{item.MiddleName}\t{item.LastName}");
+            }
+
+            int totalsum = 15000;
+            var query3 = queries.CustomersTotalBoughtOver15k(totalsum);
+            foreach (var item in query3)
+            {
+                Console.WriteLine($"{item.CustomerId}\t{item.FirstName}\t{item.MiddleName}\t{item.LastName}");
+            }
+
+            Console.Read();
+        }
+
+    }
+}
